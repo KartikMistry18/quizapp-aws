147020fd84ab723da4f17d7715fb924691de1b71
Krishay Nair <krishay958@gmail.com>
2024-04-03 22:24:19 +0530

    changes

--- src/navbar.js
+++ src/navbar.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Link from 'next/link';
 
 
 const Navbar = () => {
@@ -8,22 +7,22 @@ const Navbar = () => {
     <>
       <nav style={{ backgroundColor: '#1E40AF' }} className="py-4 px-6 flex items-center justify-between mb-5">
         <div className="flex item-center">
-          <Link href="/">
+          <a href="/">
             <div className="text-lg font-bold text-white">DiabPro</div>
-          </Link>
+          </a>
         </div>
         <div className="text-white">
-          <Link href="/index" className="text-gray-300 hover:text-white mr-4">Home</Link>
-          <Link href="/quiz" className="text-gray-300 hover:text-white mr-4">quiz</Link>
+          <a href="/index" className="text-gray-300 hover:text-white mr-4">Home</a>
+          <a href="/quiz" className="text-gray-300 hover:text-white mr-4">quiz</a>
          
-            <Link href="https://diabetesretinopathy.streamlit.app/" className="text-gray-300 hover:text-white mr-4">Diabetes Retinopathy</Link>
+            <a href="https://diabetesretinopathy.streamlit.app/" className="text-gray-300 hover:text-white mr-4">Diabetes Retinopathy</a>
           
           
-            <Link href="https://diabfordoc.streamlit.app/" className="text-gray-300 hover:text-white mr-4">Stats for doctors</Link>
+            <a href="https://diabfordoc.streamlit.app/" className="text-gray-300 hover:text-white mr-4">Stats for doctors</a>
         </div>
         <div>
           
-              <Link href="/sign-in" className="text-gray-300 hover:text-white mr-4">Sign In / Sign Up for doctors  </Link>
+              <a href="/sign-in" className="text-gray-300 hover:text-white mr-4">Sign In / Sign Up for doctors  </a>
         </div>
       </nav>
     </>
